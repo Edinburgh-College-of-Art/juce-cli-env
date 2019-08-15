90632fec5339443034dcdfadaa7ffbe98a519d08
mhamilt <matt.hamilton@ed.ac.uk>
2019-08-15 10:29:50 +0100

    restructure

--- juce-cli-env/Source/Main.cpp
+++ juce-cli-env/Source/Main.cpp
@@ -13,6 +13,13 @@
 //==============================================================================
 int main (int argc, char* argv[])
 {
+    int *a = new int;
+    *a = 1;
+    ScopedPointer<int> x = new int (2);
+    std::unique_ptr<int> y(new int (3));
+    
+    std::cout << *(x) << '\n';
+    
     AudioProcessing processor;
     printf("Press 'q' to quit:\n");
     while (getchar() != 'q')
